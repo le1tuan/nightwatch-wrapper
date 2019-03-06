640d936305aa9c75c6afaa72b6fb3ddea7827d4a
binh <binhduongquangvtca@gmail.com>
2019-03-06 10:20:24 +0700

    update

--- src/test.js
+++ src/test.js
@@ -1,18 +1,38 @@
 const fs = require('fs')
 function parseTestScript() {
-  let content = `
-  module.exports = {
-  `
   const testScriptPath = "test_script/config.json";
-  // const testScript = JSON.parse();
   fs.readFile(testScriptPath,'utf8', (err, data) => {
     const testScriptData = JSON.parse(data);
-
-    const content = `
-      console.log('aaaaaaa')
-    `
+    let contentBody = ''
+    let header = ''
+    testScriptData.test_group.map((group, indexGroup) => 
+      {
+        group.steps.map((step,index)=> {
+        const parseTestID = step.id ? JSON.stringify(`#${step.id}`) : '';
+        const value = JSON.stringify(step.value) || ''
+        const value_1 = JSON.stringify(step.value_1) || ''
+        const action = `${step.action}${step && step.action_1 ? `.${step.action_1}` : ''}`
+        if(step.action === 'pause'){
+          contentBody += `.${step.action}(${value})${breakLine}`
+        }else if(step && step.typeID === 'class') contentBody += `.${action}(${JSON.stringify(step.id)}${value ? `,${value}` : ''})\n`
+        else contentBody += `.${action}(${parseTestID}${value ? `,${value}` : ''}${value_1 ? `,${value_1}` : ''})\n`
+        })
+        header +=
+        `${JSON.stringify(group.tag)} : function(client){
+          client
+          .url(${JSON.stringify(testScriptData.url)})
+          .waitForElementVisible('body', 10000)
+          ${contentBody}
+          .pause(2000)
+          .end
+        }${indexGroup < testScriptData.test_group.length - 1 ? ',' : '' }\n`
+    })
+    const content =
+    `module.exports = {
+      ${header}
+    }`
     fs.writeFile(`tests/${testScriptData.title}.js`, content, () => {
-      console.log('ok')
+      console.log(content)
     })
   })
 }
--- tests/math.js
+++
@@ -1,8 +0,0 @@
-const config = require('../nightwatch.conf');
-const runAutomationTest = require('../src/test');
-
-module.exports = {
-  'google': function(browser) {
-    runAutomationTest(browser);
-  }
-}
