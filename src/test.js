836cdc2aae9189d76d20ba650b69ee252ae44616
] <binhduongquangvtca@gmail.com>
2019-03-08 15:21:02 +0700

    no message

--- src/test.js
+++ src/test.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 function parseTestScript() {
-  const testScriptPath = __dirname +'../../../config-automation/config.json';
+  const testScriptPath = __dirname +'/../config-automation/config.json';
   fs.readFile(testScriptPath,'utf8', (err, data) => {
     const testScriptData = JSON.parse(data);
     let contentBody = ''
