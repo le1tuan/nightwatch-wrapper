c2808c6989b22d528fd372c743300396dc2a7c29
benduong <binhduongquangvtca@gmail.com>
2019-03-12 11:01:56 +0700

    update

--- bin/auto-nightwatch.js
+++ bin/auto-nightwatch.js
@@ -5,7 +5,6 @@ const args = process.argv.slice(2);
 switch (args[0]){
   case 'start': 
   require(__dirname + '/../src/test.js')
-  // console.log(__dirname + '/../src/test.js')
   break;
   default: 
     console.log(`could not found ${args[0]} commandline`)
